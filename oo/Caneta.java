0e3a9c806efee259faed413c163ce98c30618aca
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2022-07-16 21:59:27 -0300

    adicionando um construtor e os metodos getters e setters

--- oo/Caneta.java
+++ oo/Caneta.java
@@ -2,16 +2,49 @@ public class Caneta {
     public String modelo;
     public String cor;
     private float ponta;
-    protected int carga;
-    protected boolean tampada;
+    //protected int carga;
+    private boolean tampada;
 
+    public Caneta(String m, String c, float p){ //esse é o metodo construtor
+        this.setModelo(m);
+        this.setCor(c);
+        this.setPonta(p);
+        this.tampar();
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+    public String getCor(){
+        return this.cor;
+    }
+    public void setCor(String c){
+        this.cor = c;
+    }
+    public float getPonta(){
+        return this.ponta;
+    }
+    public void setPonta( float p){
+        this.ponta = p;
+    }
+    
     public void status(){
        System.out.println("Modelo: "+this.modelo);
-       System.out.println("Uma caneta: "+this.cor); 
+       System.out.println("cor: "+this.cor); 
        System.out.println("ponta: "+this.ponta);
-       System.out.println("carga: "+this.carga);
+       //System.out.println("carga: "+this.carga);
        System.out.println("está tampada? "+this.tampada);
     }
+    public void tampar(){
+        this.tampada = true;
+    }
+    public void destampar(){
+        this.tampada = false;
+    }
+    /* 
     public void rabiscar(){
         if(this.tampada == true){
            System.out.println("ERRO! Não posso rabiscar"); 
@@ -19,10 +52,8 @@ public class Caneta {
             System.out.println("Estou rabiscando");
         }
     }
-    protected void tampar(){
-        this.tampada = true;
-    }
     protected void destampar(){
         this.tampada = false;
     }
+    */
 }
--- oo/Main.java
+++ oo/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args){
-      Caneta c1 = new Caneta();
-      c1.modelo = "bic";
-      //c1.ponta = 0.5f;
+      Caneta c1 = new Caneta("nic","amarelo",0.04f);
       c1.status();
     }
 }
